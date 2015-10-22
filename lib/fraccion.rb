8bd78dc5355c8b146b25da36f6f3ec913b52a2bb
hectorinoman <hectorinoman@gmail.com>
2015-10-22 23:38:58 +0000

    multiplicacion2

--- lib/fraccion.rb
+++ lib/fraccion.rb
@@ -1,8 +1,16 @@
 class Fraccion
   
-  attr_reader :x, :y
-  def initialize(x, y)
-    @x, @y = x, y
+  attr_reader :num, :dem
+  def initialize(num, dem)
+    @num, @dem = num, dem
+  end
+  
+  def to_s
+    "(#{@num},#{@dem})"
+  end
+  
+  def *(value)
+    Fraccion.new(@num * value, @dem * value)
   end
   
 end
