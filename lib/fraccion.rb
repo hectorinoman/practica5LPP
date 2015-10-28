7a1d7048b1568b0a9edd3a8344ccf229c701f790
hectorinoman <hectorinoman@gmail.com>
2015-10-28 23:52:36 +0000

    mejoraSumaConDenominadoresDiferentes

--- lib/fraccion.rb
+++ lib/fraccion.rb
@@ -9,10 +9,12 @@ class Fraccion
     "(#{@num},#{@dem})"
   end
   
-  def mcm(num,dem)
-    Fraccion.new(@num * other.dem, @dem * other.dem)
-    
-  end  
+  def + (other)
+    mcm = @dem.lcm(other.dem)
+    num1 = (mcm * @num) / @dem
+    num2 = (mcm * other.num) / other.dem
+    Fraccion.new(num1+num2, mcm)
+  end
   
   
   
@@ -26,9 +28,7 @@ class Fraccion
     Fraccion.new(@num * -1, @dem * -1)
   end
   
-  def + (other)
-    Fraccion.new(@num + other.num, @dem)
-  end
+
   
   def - (other)
     Fraccion.new(@num - other.num, @dem - other.dem)
--- test/tc_fraccion.rb
+++ test/tc_fraccion.rb
@@ -18,11 +18,16 @@ class TestFraccion < Test::Unit::TestCase
   def test_simple
     assert_equal("(5,4)", @origen.to_s)
     #assert_equal("(10,25)", (@unidad*5).to_s) #multiplicacion por constante
+    assert_equal("(33,20)", (@origen + @unidad).to_s)  
     assert_equal("(-2,-5)", (-@unidad).to_s)
-    assert_equal("(18,4)", (@test1 + @test2).to_s)  
     assert_equal("(3,-1)", (@origen - @unidad).to_s)
     assert_equal("(10,20)", (@origen * @unidad).to_s)  ##multiplicacion de fracciones
     assert_equal("(400,600)", (@prueba1 ** @prueba2).to_s)  ##division de fracciones
   end
+  
+  def suma_resta_mcm
+    
+  end
+    
 
 end
